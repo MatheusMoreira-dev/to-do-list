6c30a93998b86cf86a313ba24dc795a5a8284ed8
MatheusMoreira-dev <matheus.moreira.marcelino.dev@gmail.com>
2026-03-10 12:47:15 -0300

    estilizando a tabela

--- app.js
+++ app.js
@@ -1,159 +1,168 @@
 const tableTasks = document.querySelector("#tasks tbody");
 const btnAdd = document.getElementById("add-row");
 
-class SelectTags{
-    static defaultOptions = [
-        {name: "", value: ""},
-        {name: "Trabalho", value: "trabalho"}, 
-        {name: "Faculdade", value: "faculdade"}, 
-        {name: "Mercado", value:"mercado"}, 
-        {name: "Revisão", value: "revisão"}
-    ];
+class SelectTags {
+  static defaultOptions = [
+    { name: "", value: "" },
+    { name: "Trabalho", value: "trabalho" },
+    { name: "Faculdade", value: "faculdade" },
+    { name: "Mercado", value: "mercado" },
+    { name: "Revisão", value: "revisão" },
+  ];
 
-    constructor(){
-        this.element = document.createElement("select");
+  constructor() {
+    this.element = document.createElement("select");
 
-        for (let option of SelectTags.defaultOptions){
-            this.addLabel(option);
-        }
+    for (let option of SelectTags.defaultOptions) {
+      this.addLabel(option);
     }
+  }
 
-    addLabel({name, value}){
-        const option = document.createElement("option");
-        option.label = name;
-        option.value = value;
+  addLabel({ name, value }) {
+    const option = document.createElement("option");
+    option.label = name;
+    option.value = value;
 
-        this.element.append(option);
-    }
+    this.element.append(option);
+  }
 }
 
-class Task{
-    constructor({id, name = "", isCompleted = false, tag = ""}){
-        this.id = id;
-        this.name = name;
-        this.isCompleted = isCompleted;
-        this.tag = tag;
-    }
+class Task {
+  constructor({ id, name = "", isCompleted = false, tag = "" }) {
+    this.id = id;
+    this.name = name;
+    this.isCompleted = isCompleted;
+    this.tag = tag;
+  }
 }
 
-class TaskStorage{
-    static key = "tasks";
+class TaskStorage {
+  static key = "tasks";
 
-    static refreshStorage(updateList){
-        localStorage.setItem(TaskStorage.key, JSON.stringify(updateList));
+  static refreshStorage(updateList) {
+    localStorage.setItem(TaskStorage.key, JSON.stringify(updateList));
+  }
+
+  static getAll() {
+    const items = localStorage.getItem(TaskStorage.key);
+
+    return items ? JSON.parse(items) : [];
+  }
+
+  static add(task) {
+    const allTasks = this.getAll();
+    task["id"] = crypto.randomUUID();
+
+    allTasks.push(task);
+
+    this.refreshStorage(allTasks);
+  }
+
+  static delete(id) {
+    const tasks = this.getAll();
+    const foundIndex = tasks.findIndex((v) => v.id == id);
+
+    if (foundIndex != -1) tasks.splice(foundIndex, 1);
+    this.refreshStorage(tasks);
+  }
+
+  static patch(id, payload) {
+    const tasks = this.getAll();
+    const foundIndex = tasks.findIndex((v) => v.id == id);
+
+    if (foundIndex != -1) {
+      for (let key in payload) tasks[foundIndex][key] = payload[key];
+      this.refreshStorage(tasks);
     }
 
-    static getAll(){
-        const items = localStorage.getItem(TaskStorage.key);
-
-        return items ? JSON.parse(items) : [];
-    }
-
-    static add (task) {
-        const allTasks = this.getAll();
-        task["id"] = crypto.randomUUID();
-
-        allTasks.push(task);
-
-        this.refreshStorage(allTasks);
-    }
-
-    static delete(id){
-        const tasks = this.getAll();
-        const foundIndex = tasks.findIndex(v => v.id==id);
-
-        if(foundIndex != -1) tasks.splice(foundIndex, 1);
-        this.refreshStorage(tasks);
-    }
-
-    static patch(id, payload){
-        const tasks = this.getAll();
-        const foundIndex = tasks.findIndex(v => v.id == id);
-
-        if(foundIndex != -1){
-            for(let key in payload) tasks[foundIndex][key] = payload[key];
-            this.refreshStorage(tasks);
-        }
-
-        return tasks[foundIndex];
-    }
+    return tasks[foundIndex];
+  }
 }
 
-class TaskRow{
-    constructor(task){
-        this.task = task;
-        this.element = document.createElement("tr");
-        
-        this.element.append(
-            this.colCheck(task.isCompleted), 
-            this.colName(task.name), 
-            this.colTag(task.tag),
-            this.colTrash()
-        );
-    }
+class TaskRow {
+  constructor(task) {
+    this.task = task;
+    this.element = document.createElement("tr");
 
-    colCheck(isCompleted){
-        const col = document.createElement("td");
-        
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = isCompleted;
-        checkbox.value = isCompleted;
-        
-        checkbox.onclick = (e) => {
-            const boolValue = e.target.checked;
-            const patchTask = TaskStorage.patch(this.task.id, {"isCompleted": boolValue});
-            
-            e.target.value = boolValue;
-            this.task = patchTask;
-        };
-        
-        col.append(checkbox);
+    this.element.append(
+      this.colCheck(task.isCompleted),
+      this.colName(task.name),
+      this.colTag(task.tag),
+      this.colTrash(),
+    );
+  }
 
-        return col;
-    }
+  colCheck(isCompleted) {
+    const col = document.createElement("td");
+    col.classList.add("col-status");
 
-    colName(name){
-        const col = document.createElement("td");
-        col.contentEditable = true;
-        col.textContent = name;
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = isCompleted;
+    checkbox.value = isCompleted;
 
-        return col;
-    }
+    checkbox.onclick = (e) => {
+      const boolValue = e.target.checked;
+      const patchTask = TaskStorage.patch(this.task.id, {
+        isCompleted: boolValue,
+      });
 
-    colTag(tag){
-        const col = document.createElement("td");
-        const select = new SelectTags();
-        
-        select.element.value = tag;
-        this.task.tag = tag;
+      e.target.value = boolValue;
+      this.task = patchTask;
+    };
 
-        select.element.onchange(e => e);
+    col.append(checkbox);
 
-        col.append(select.element);
-        return col;
-    }
+    return col;
+  }
 
-    colTrash(){
-        const btn = document.createElement("button");
-        btn.innerText = "Excluir";
-        btn.onclick = () => this.deleteRow();
+  colName(name) {
+    const col = document.createElement("td");
+    col.contentEditable = true;
+    col.textContent = name;
 
-        return btn;
-    }
+    col.addEventListener("blur", (e) => {
+      TaskStorage.patch(this.task.id, { name: e.target.textContent });
+    });
 
-    deleteRow(){
-        this.element.remove();
-        TaskStorage.delete(this.task.id);
-    }
+    return col;
+  }
+
+  colTag(tag) {
+    const col = document.createElement("td");
+    const select = new SelectTags();
+
+    select.element.value = tag;
+    this.task.tag = tag;
+
+    col.append(select.element);
+    return col;
+  }
+
+  colTrash() {
+    const col = document.createElement("td");
+
+    const img = document.createElement("img");
+    img.classList.add("trash");
+    img.src = "./assets/trash.svg";
+    img.onclick = () => this.deleteRow();
+
+    col.appendChild(img);
+    return col;
+  }
+
+  deleteRow() {
+    this.element.remove();
+    TaskStorage.delete(this.task.id);
+  }
 }
 
-function renderSaveTasks(){
-    const tasks = TaskStorage.getAll().map(v => new Task(v));
-    
-    for (let task of tasks){
-        tableTasks.prepend(new TaskRow(task).element);        
-    }
+function renderSaveTasks() {
+  const tasks = TaskStorage.getAll().map((v) => new Task(v));
+
+  for (let task of tasks) {
+    tableTasks.prepend(new TaskRow(task).element);
+  }
 }
 
 renderSaveTasks();
@@ -161,13 +170,13 @@ renderSaveTasks();
 let i = 1;
 
 btnAdd.addEventListener("click", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const emptyTask = new Task({name: i});
-    const row = new TaskRow(emptyTask);
-    
-    TaskStorage.add(emptyTask);
+  const emptyTask = new Task({ name: i });
+  const row = new TaskRow(emptyTask);
 
-    tableTasks.prepend(row.element);
-    i++;
-}); 
+  TaskStorage.add(emptyTask);
+
+  tableTasks.prepend(row.element);
+  i++;
+});
